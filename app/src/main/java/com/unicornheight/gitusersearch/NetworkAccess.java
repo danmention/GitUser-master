5a3237aa52bf0effa3b588106558dd3420f34478
danmention <ughegheehiosu@gmail.com>
2017-08-30 10:23:50 +0100

    initial commit

---
+++ app/src/main/java/com/unicornheight/gitusersearch/NetworkAccess.java
@@ -0,0 +1,64 @@
+package com.unicornheight.gitusersearch;
+
+
+import android.net.Uri;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+
+
+public class NetworkAccess {
+
+
+    final static String GITHUB_URL =
+            "https://api.github.com/search/users";
+
+    final static String PARAM_QUERY = "q";
+
+    final static String PARAM_SORT = "sort";
+
+    final static String sortBy = "stars";
+
+    public static URL buildGitUrl(String jointQuery) {
+        Uri builtUri = Uri.parse(GITHUB_URL).buildUpon()
+                .appendQueryParameter(PARAM_QUERY, jointQuery)
+                .appendQueryParameter(PARAM_SORT, sortBy)
+                .appendQueryParameter(BuildConfig.TOKEN_API_KEY, null)
+                .build();
+
+        URL url = null;
+        try {
+            url = new URL(builtUri.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        return url;
+    }
+
+
+    public static String getResponseFromHttpUrl(URL githubUrl) throws IOException {
+        HttpURLConnection urlConnection = (HttpURLConnection) githubUrl.openConnection();
+        try {
+            InputStream in = urlConnection.getInputStream();
+
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+            if (hasInput) {
+                return scanner.next();
+            } else {
+                return null;
+            }
+        } finally {
+            urlConnection.disconnect();
+        }
+    }
+
+
+}
