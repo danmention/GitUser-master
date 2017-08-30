b4379e814b4e2ef34abedbba0e884363a87f5098
danmention <ughegheehiosu@gmail.com>
2017-08-30 12:42:59 +0100

    initial commit

--- app/src/main/java/com/unicornheight/gitusersearch/MainActivity.java
+++ app/src/main/java/com/unicornheight/gitusersearch/MainActivity.java
@@ -7,14 +7,14 @@ import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.AsyncTask;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
 import java.net.URL;
 
 
@@ -24,6 +24,7 @@ public class MainActivity extends AppCompatActivity implements GitUserAdapter.Gi
     TextView mError;
     Button refreshButton;
     private GitUserAdapter mAdapter;
+    // good
 
 
     @Override
